ddc9c937d1fd916193d9235ee420db8ee2c622b6
prashant kumar <prashantdoa1996@gmail.com>
2023-09-25 16:50:59 +0530

    files added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// components
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNewTodo } from '../redux/actions';
+
+const TodoForm = () => {
+    // eslint-disable-next-line
+    const [text, setText] = useState('');
+    const dispatch = useDispatch();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        dispatch(addNewTodo(text));
+
+        setText('');
+    };
+
+    const onFormChange = (e) => {
+        // console.log(e.target.value);
+        setText(e.target.value);
+    };
+
+
+    return (
+        <form
+            onSubmit={onFormSubmit}
+            className='form'
+        >
+            <input
+                onChange={onFormChange}
+                className='input'
+                type="text"
+                value={text}
+                placeholder='Enter new todo...' />
+        </form>
+    )
+}
+
+export default TodoForm
