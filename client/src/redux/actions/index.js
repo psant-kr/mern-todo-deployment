b475c299ee996dc91b0db4cc51211ec1aad56a03
prashant kumar <prashantdoa1996@gmail.com>
2023-09-26 04:29:08 +0530

    api url and credentials added to the redux file

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { ADDNEW_TODO } from './type';
 
 // const API_URL = 'https://localhost:8000';
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = 'https://mern-todo-deployment-api.vercel.app'
+
+axios.defaults.withCredentials = true;
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
