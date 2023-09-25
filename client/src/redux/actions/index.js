b19600f312483853bbae9679baa3c4887c08e03b
prashant kumar <prashantdoa1996@gmail.com>
2023-09-26 04:57:28 +0530

    trying to resolve the cors error

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -3,11 +3,12 @@ import { ADDNEW_TODO } from './type';
 
 // const API_URL = 'https://localhost:8000';
 // const API_URL = 'http://localhost:8000';
-const API_URL = 'https://mern-todo-deployment-api.vercel.app'
+const API_URL = 'https://mern-todo-deployment-api.vercel.app';
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 export const addNewTodo = (data) => async (dispatch) => {
+    axios.defaults.withCredentials = true;
     try {
         const res = await axios.post(`${API_URL}/todos`, { data })
         dispatch({ type: ADDNEW_TODO, payload: res.data });
