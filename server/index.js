dcda8564179d38425c5b72a46157020c71c31140
prashant kumar <prashantdoa1996@gmail.com>
2023-09-26 04:40:47 +0530

    frontend address url added to the origin of server file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: [],
+        origin: ["https://mern-todo-deployment-frontend.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
