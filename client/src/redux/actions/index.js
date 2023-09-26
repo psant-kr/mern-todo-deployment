c3dfa96d58f87120b67dc087c24992e1bdbfc90e
prashant kumar <prashantdoa1996@gmail.com>
2023-09-26 16:41:17 +0530

    cors error

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import { ADDNEW_TODO } from './type';
 
+axios.defaults.withCredentials = true;
+
 // const API_URL = 'https://localhost:8000';
 // const API_URL = 'http://localhost:8000';
 const API_URL = 'https://mern-todo-deployment-api.vercel.app';
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 export const addNewTodo = (data) => async (dispatch) => {
-    
+
     try {
         const res = await axios.post(`${API_URL}/todos`, { data })
         dispatch({ type: ADDNEW_TODO, payload: res.data });
