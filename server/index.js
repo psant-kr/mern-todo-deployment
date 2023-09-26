234981d4944e397e525db4d15bcce60f76c09ecc
prashant kumar <prashantdoa1996@gmail.com>
2023-09-26 18:02:09 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -20,8 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', Routes);
 
-const PORT = 8000;
+// const PORT = 8000;
+const port = process.env.PORT || 8000;
 
 Connection();
 
-app.listen(PORT, () => console.log(`Your server is running sucessfully on PORT ${PORT}`));
+app.listen(port, () => console.log(`Your server is running sucessfully on PORT ${PORT}`));
